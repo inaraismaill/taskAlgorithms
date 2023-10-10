30134446610b612907434d1234e94784b043925d
inaraismaill <mi7q3xpdl@code.edu.az>
2023-10-10 21:24:49 +0400

    consoleApp2 elave olundu

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Verilmiş arrayı tərsinə çevirib yeni array yaradın
+
+int[] oldarray= { 23, 4, 64, 2, 67, 53 };
+int extent = oldarray.Length;
+int[] newarray = new int[extent];
+for (int i = 0; i < oldarray.Length; i++)
+{
+    newarray[i] = oldarray[extent-1-i];
+    Console.Write(newarray[i]+" ");
+}
